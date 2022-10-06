048e86c3b53a85615c964831f18672da3dc6826e
James Njenga <knjenga94@gmail.com>
2022-10-06 14:11:27 +0300

    Added a vertical line component to the experience page

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,9 +1,24 @@
-
+import React from "react"
+import { 
+    VerticalTimeline, 
+    VerticalTimelineElement 
+}from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import SchoolIcon from "@material-ui/icons/School"
 function Experience(){
     return(
-        <di>
-            <h1>My Experience</h1>
-        </di>
+        <div className="experience">
+            <VerticalTimeline lineColor="#3e497a">
+<VerticalTimelineElement 
+className="vertical-timeline-eleme"
+date="July 2022"
+    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+    icon={<SchoolIcon />}>
+        <h3 className="vertical-timeline-element-title">Moringa School, Ngong Lane Plaza, Nairobi Kenya</h3>
+    <p>Fullstack Software Development (React, Ruby on rails)</p>
+    </VerticalTimelineElement> 
+            </VerticalTimeline>
+        </div>
     )
 }
 
