a43e85df98d0e1ddd49dcb4a06cfbf3a51fed72f
James Njenga <knjenga94@gmail.com>
2022-10-06 13:40:02 +0300

    Styled the homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,34 +3,37 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import EmailIcon from '@mui/icons-material/Email';
+import "../styles/Home.css"
 function Home() {
     return (
         <div className="home">
-            <div className="about"></div>
-            <h1>Hello,My Name is James</h1>
-            <div className="prompt">
-                <p>
-                    A Software Developer With a passion
-                    for learning and creating
-                </p>
-                <LinkedInIcon />
-                <EmailIcon />
-                <GitHubIcon />
+            <div className="about">
+                <h2>Hello,My Name is James</h2>
+
+                <div className="prompt">
+                    <p>
+                        A Software Developer With a passion
+                        for learning and creating
+                    </p>
+                    <LinkedInIcon />
+                    <EmailIcon />
+                    <GitHubIcon />
+                </div>
             </div>
-            <div className="skills"></div>
+            <div className="skills">
             <h1>Skills</h1>
             <ol className="list">
                 <li className="item">
                     <h2>Front-End</h2>
                     <span>
-                    ReactJs, Tailwind Css, HTML, Bootstrap, MaterialUI,
-                    StyledComponents
+                        ReactJs, Tailwind Css, HTML, Bootstrap, MaterialUI,
+                        StyledComponents
                     </span>
                 </li>
                 <li className="item">
                     <h2>Back-End</h2>
                     <span>
-                    Ruby, Rails, Python, MongoDb, AWS, MySQL, NodeJs/Express
+                        Ruby, Rails, Python, MongoDb, AWS, MySQL, NodeJs/Express
                     </span>
                 </li>
                 <li className="item">
@@ -38,10 +41,10 @@ function Home() {
                     <span>Ruby, JavaScript, Python</span>
                 </li>
             </ol>
-
-            
         </div>
-    )
+</div >
+        
+    );
 }
 
 export default Home;
