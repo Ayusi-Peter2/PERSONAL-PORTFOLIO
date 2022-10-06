04b809f053434be66ce46329aaf2f8652ff7e26a
James Njenga <knjenga94@gmail.com>
2022-10-06 18:52:58 +0300

    Addeda ProjectItem component and image assets

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,16 +1,22 @@
-import Navbar from "../components/NavBar";
-import Footer from "../components/Footer";
+import ProjectItem from "../components/ProjectItem";
+import proj1 from "../assets/proj1.jpg";
+import proj2 from "../assets/proj2.jpg";
 function Projects(){
     return(
-        <div >
+        <div className="projects">
           
-            <h1>Some of my projects include: </h1>
-            <ul>
+            <h1>My Personal Projects </h1>
+            <div className="projectList">
+<ProjectItem name="Social Media Website" image={proj1}/>
+<ProjectItem name="Dating App" image={proj2}/>
+
+            </div>
+            {/* <ul>
                 <li>Weather Application</li>
                 <li>Smart Meter Reader</li>
                 <li>Dating App</li>
                 <li>School Management System</li>
-            </ul>
+            </ul> */}
            
         </div>
     )
