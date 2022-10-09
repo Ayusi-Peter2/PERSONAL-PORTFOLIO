6465f20f91f6a80f145eebfdf5c75195c95ed325
James Njenga <knjenga94@gmail.com>
2022-10-09 12:26:29 +0300

    Created an item component to display a single project

---
+++ src/components/Item.jsx
@@ -0,0 +1,30 @@
+import React, { useState,useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
+
+function Item() {
+const param=useParams()
+
+const[dataSearch,setDataSearch]=useState({})
+
+useEffect(() => {
+  fetch(`http://localhost:3000/ProjectList/${param.id}`)
+    .then((response) => response.json())
+    .then(projects => {
+  
+      setDataSearch(dataSearch =>projects)
+    })
+}, [])
+
+console.log(dataSearch)
+  return (
+    <div>
+<img src={dataSearch.image} />
+<h1>{dataSearch.name}</h1>
+<h1>Skills : {dataSearch.skills}</h1>
+
+    </div>
+  )
+}
+
+export default Item
