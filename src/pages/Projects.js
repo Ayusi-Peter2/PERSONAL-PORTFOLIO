f5d7a577a16d26edf19350863b4944d1d97b3b3f
James Njenga <knjenga94@gmail.com>
2022-10-09 12:38:02 +0300

    Made changes to projects file to accomodate props from the App.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,19 +1,48 @@
+import React,{useState,useEffect} from "react";
 import ProjectItem from "../components/ProjectItem";
 import { ProjectList } from "../helpers/ProjectList"
 import "../styles/Projects.css"
-function Projects() {
+
+function Projects({allProjects}) {
+    // const [allProjects, setAllProjects] = useState([])
+
+    // //const [deleteFn, setDelete] = useState(false)
+
+    // useEffect(() => {
+    //     fetch("http://localhost:3000/ProjectList")
+    //         .then((response) => response.json())
+    //         .then(projects => {
+    //             //console.log(projects)
+    //             setAllProjects(allProjects => projects)
+    //         })
+    // }, [])
+
+    // console.log(allProjects)
+
     return (
         <div className="projects">
 
             <h1>My Personal Projects </h1>
             <div className="projectList">
-                {ProjectList.map((project,index) => {
-                    return <ProjectItem id={index} name={project.name}
-                    image={project.image}/>
+                {allProjects.map((project,index) => {
+                 
+                    return <ProjectItem key={index} id={project.id} name={project.name}
+                    image={project.image} allProjects={allProjects}/>
                 })}
 
             </div>
         </div>
+        // <div className="projects">
+
+        //     <h1>My Personal Projects </h1>
+        //     <div className="projectList">
+        //         {ProjectList.map((project,index) => {
+        //             return <ProjectItem id={index} name={project.name}
+        //             image={project.image}/>
+        //         })}
+
+        //     </div>
+        // </div>
     )
 }
 
